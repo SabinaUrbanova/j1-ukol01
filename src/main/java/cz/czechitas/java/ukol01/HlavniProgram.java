a651bce928a4869fd2b3597becc4c9945b3e7a7e
Sabina Urbanova <urbanova.sabina@gmail.com>
2024-01-27 12:25:04 +0100

    druh8 část úkolu

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -11,7 +11,16 @@ public class HlavniProgram {
 
   public void start() {
     zofka = new Turtle();
+    posun_na_zacatek();
     nakresli_prasatko(100);
+     posun(250);
+    nakresli_n_uhelnik(8,30);
+    posun(150);
+    nakresli_n_uhelnik(20,20);
+    posun(100);
+    nakresli_slunicko(30, 50);
+
+
   }
 
   public void nakresli_ctverec(int delka_strany) {
@@ -20,8 +29,6 @@ public class HlavniProgram {
       zofka.turnRight(90);
     }
   }
-
-
 
   public void nakresli_domecek(int delka_strany) {
     nakresli_ctverec(delka_strany);
@@ -62,8 +69,49 @@ public class HlavniProgram {
     zofka.turnRight(90);
     zofka.move(delka_strany);
     nakresli_nozicky(100, 25);
+    zofka.turnRight(45);
 
   }
+
+  public void posun_na_zacatek() {
+    zofka.penUp();
+    zofka.move(150);
+    zofka.turnLeft(90);
+    zofka.move(300);
+    zofka.turnRight(90);
+    zofka.penDown();
+  }
+
+  public void posun(int delka_posunu) {
+    zofka.penUp();
+    zofka.turnRight(90);
+    zofka.move(delka_posunu);
+    zofka.turnLeft(90);
+    zofka.penDown();
+  }
+
+
+
+  public void nakresli_n_uhelnik(int pocet_uhlu, int delka_strany) {
+    for (int i = 0; i < pocet_uhlu; i++) {
+      zofka.move(delka_strany);
+      zofka.turnLeft(360/pocet_uhlu);
+    }
+
+  }
+
+  public void nakresli_slunicko(int delka_strany, int delka_luce) {
+    for (int i = 0; i < 15; i++) {  //počet opakování (i) x uhel = 180
+      zofka.move(delka_strany);
+      zofka.turnRight(360/30);
+      zofka.turnLeft(90);
+      zofka.move(delka_luce);
+      zofka.turnLeft(180);
+      zofka.move(delka_luce);
+      zofka.turnLeft(90);
+      zofka.turnRight(360/30);
+  }
+}
 
 }
 
